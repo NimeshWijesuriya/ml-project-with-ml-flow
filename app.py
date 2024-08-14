59bdaed5e96f314694c141ded765ab990d84e94b
nimeshwijesuriya <2019s17521@stu.cmb.ac.lk>
2024-08-14 21:11:04 +0530

    app.py added

--- app.py
+++ app.py
@@ -136,4 +136,5 @@ def train_model():
     pass
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+   # app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080)
